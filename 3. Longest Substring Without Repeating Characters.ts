5aeaf4bb23c5e1dc4563aef58ee7b0da4cc5796e
thuang86714 <thuang0714@gmail.com>
2024-03-01 15:53:28 +0800

    fix: 3.Longest...

--- 3. Longest Substring Without Repeating Characters.ts
+++ 3. Longest Substring Without Repeating Characters.ts
@@ -1,11 +1,11 @@
 function lengthOfLongestSubstring(s: string): number {
     //TC O(n^2), SC O(1)
     const dict: boolean[] = Array(128).fill(false);
-    let length: number = 0;
-    let ans: number = 0;
+    let length = 0;
+    let ans = 0;
 
     for (let i = 0, j = 0; i < s.length; i++) {
-        const index: number = s.charCodeAt(i);
+        const index = s.charCodeAt(i);
         
         if (dict[index]) {
             while (dict[index]) {
